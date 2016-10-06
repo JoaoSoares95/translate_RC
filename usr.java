1cf55cdc7d1a0edc9ef9a56791c8e8021061366b
JoaoSoares95 <joao_soares_95@hotmail.com>
2016-10-06 19:05:00 +0100

    While cycle but some troubles(Cleaning buffer). Compiles, though

--- usr.java
+++ usr.java
@@ -2,43 +2,55 @@ import java.io.*;
 import java.net.*;
 
 
-
 public class usr{
-/*
-	//variable definition;
-	private int GN;
-	private int TRSport;
-	private int TCSport;
-	
-	
-	//methods for usr;
-	public void usr(int GN_c, int TRSport_c, int TCSport_c){
-		GN=GN_c;
-		TRSport=TRSport_c;
-		TCSport=TCSport_c;
-	}
+
+	public static void main(String args[]) throws Exception{
+	   
+	    int GN=45;//Group number
+		int TCSport=58000+GN;//Port to TCS if not in parameters.
+	   
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName("localhost");
+		  
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
 		
-}
+		String sentence1 = inFromUser.readLine();
+		
+		//while message not "exit";
+		while (!(sentence1.equals("exit"))){
+			//read input from comand line;
+			String sentence = inFromUser.readLine();
+			sendData = sentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence);
+			
+			//list command
+			if (sentence.equals("list")){
+				
+				//read input from comand line;
+				String lang_select = inFromUser.readLine();
+				sendData = lang_select.getBytes();
+				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
+				clientSocket.send(sendPacket);
+				receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+				String modified_language = new String(receivePacket.getData());
+				System.out.println("Lang_Selected: " + modified_language);
+				
+			}
+			else if (sentence.equals("exit")){
+				
+				sentence1 = sentence;
 
-import java.io.*;
-import java.net.*;
+			}
+		}
+		clientSocket.close();
 
-class UDPClient{*/
-   public static void main(String args[]) throws Exception
-   {
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-      clientSocket.send(sendPacket);
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
-      String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + modifiedSentence);
-      clientSocket.close();
    }
 }
