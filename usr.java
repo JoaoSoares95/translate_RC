deac22c9701b8ccb15c6441a65eef1c343dabc98
JoaoSoares95 <joao_soares_95@hotmail.com>
2016-10-07 12:20:28 +0100

    All options work properly, but what they should do exactly is not coded yet

--- usr.java
+++ usr.java
@@ -16,41 +16,44 @@ public class usr{
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 		
-		String sentence1 = inFromUser.readLine();
+		/*String sentence1 = inFromUser.readLine();
 		sendData = sentence1.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
-		clientSocket.send(sendPacket);
-		
+		clientSocket.send(sendPacket);		
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 		clientSocket.receive(receivePacket);
 		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
+		System.out.println("FROM SERVER:" + modifiedSentence);*/
 		
 		
 		//while message not "exit";
-		while (!(sentence1.equals("exit"))){
+		/*while (!(sentence1.equals("exit"))){*/
+		while (true){
+		
 			//read input from comand line;
 			String sentence = inFromUser.readLine();
+			
+			
 			sendData = sentence.getBytes();
-			/*DatagramPacket*/ sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
 			//clientSocket.send(sendPacket);
 
-			receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			modifiedSentence = new String(receivePacket.getData());
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			//clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
 			
 			//list command
 			if (sentence.equals("list")){
 				
-				//read input from comand line;
-				/*String lang_select = inFromUser.readLine();
-				sendData = lang_select.getBytes();*/
 				sendData = "ULQ".getBytes();
-				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
+				
+				/*DatagramPacket*/ sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, TCSport);
 				clientSocket.send(sendPacket);
+				
 				receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				clientSocket.receive(receivePacket);
 				String modified_language = new String(receivePacket.getData());
+				
 				System.out.println("Lang_Selected: " + modified_language);
 				
 			}
@@ -58,19 +61,37 @@ public class usr{
 			//request command
 			else if (sentence.equals("request")){
 				
+				clientSocket.send(sendPacket);
+				
+				receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+				String modified_language = new String(receivePacket.getData());
+				
+				System.out.println("Lang_Selected: " + modified_language);
 			}
 			
 			//exit command
 			else if (sentence.equals("exit")){
-				sentence1 = sentence;
+				//sentence1 = sentence;
+				System.out.println("Leaving");
+				clientSocket.send(sendPacket);
+
+				break;
 				
 			}
 			
 			//everything else
 			else{
-				System.out.println("FROM SERVER:" + modifiedSentence);
+				
 				clientSocket.send(sendPacket);
+				
+				clientSocket.receive(receivePacket);
+				modifiedSentence = new String(receivePacket.getData());
+				
+				System.out.println("FROM SERVER:" + modifiedSentence);
+				
 			}
+		
 		}
 		clientSocket.close();
 
