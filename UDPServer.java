12d2c0f8eb23da80f506f8da6260a0eb9f49308d
nadia-mourao <nadialm_16@hotmail.com>
2016-10-10 09:49:18 +0100

    if's SRG

--- UDPServer.java
+++ UDPServer.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.lang.*;
 import java.*;
+import java.util.*;
 
 class UDPServer{
 
@@ -12,9 +13,15 @@ class UDPServer{
                   tcs_port = Integer.parseInt(args[1]);
             }
 
-            File yourFile = new File("languages.txt");
-            yourFile.createNewFile(); // if file already exists will do nothing 
-            FileOutputStream oFile = new FileOutputStream(yourFile, false);
+            int n_languagues=0;
+            String m_ok="OK\n", m_nok="NOK\n", m_err = "ERR\n";
+            String m_sur="SUR ", m_srr="SRR ";
+
+            ArrayList <ArrayList <String>> languages = new ArrayList <ArrayList <String>>();
+
+            //File yourFile = new File("languages.txt");
+            //yourFile.createNewFile(); // if file already exists will do nothing 
+            //FileOutputStream oFile = new FileOutputStream(yourFile, false);
             
             DatagramSocket serverSocket = new DatagramSocket(tcs_port);
                   
@@ -28,7 +35,7 @@ class UDPServer{
                   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                   serverSocket.receive(receivePacket);
 
-                  System.out.println("receiveData : ----------------- " + receiveData);
+                  //System.out.println("receiveData : ----------------- " + receiveData);
 
                   sentence = new String( receivePacket.getData());
 
@@ -38,7 +45,7 @@ class UDPServer{
 
                   InetAddress IPAddress = receivePacket.getAddress();
 
-                  System.out.println("IPAddress: " + IPAddress);
+                  //System.out.println("IPAddress: " + IPAddress);
 
                   //----------------------------------------------------
 
@@ -46,12 +53,12 @@ class UDPServer{
                    *          Client          *
                    ****************************/
 
-                  if (help[0].equals("ULQ")){
-                        System.out.println("ULQ");
-                  }
-
-                  else if (help[0].equals("ULR")){
-                        System.out.println("ULR");
+                  if (help[0].equals("ULQ")){               //enviar a lista de linguagens
+                        System.out.println("ULQ");          //
+                  }                                         //
+                                                            //
+                  else if (help[0].equals("ULR")){          // ULR nL L1 L2 … LnL
+                        System.out.println("ULR");          //
                   }
 
                   else if (help[0].equals("UNQ")){
@@ -62,17 +69,47 @@ class UDPServer{
                         System.out.println("UNR");
                   }
 
+                  /****************************
+                   *         Tradutor         *
+                   ****************************/   
 
+                  else if (help[0].equals("SRG")){
+                        System.out.println("SRG");
+                        
+                        if (help.length==4){   
+                              ArrayList <String> language = new ArrayList<String>();
+                              
+                              for (int i=0; i<4; i++) {
+                                    for (int j=0; i<n_languagues; j++) {
+                                          languages
+                                    }
+                                    language.add(help[i]);
 
+                              }
+                              languages.add(language);
+                              n_languagues++;
+                        }
+                  }
 
+                  else if (help[0].equals("SRR")){
+                        System.out.println("SRR");
+                  }
 
+                  else if (help[0].equals("SUN")){
+                        System.out.println("SUN");
+
+                  }
+
+                  else if (help[0].equals("SUR")){
+                        System.out.println("SUR");
+                  }
 
 
                   //--------------------------------------------------
 
                   int port = receivePacket.getPort();
 
-                  System.out.println("port: " + port);
+                  //System.out.println("port: " + port);
 
                   String capitalizedSentence = sentence.toUpperCase();
 
@@ -85,18 +122,18 @@ class UDPServer{
                   DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                   serverSocket.send(sendPacket);
 
-                  System.out.println("sendPacket: " + sendPacket);
+                  //System.out.println("sendPacket: " + sendPacket);
 
                   if (help[0].equals("exit") && help.length==1) {
                         System.out.println("Saiu");
                         break;
                   }
 
-                  else{
+                  /*else{
                         System.out.println("--------------- " + help[0].equals("exit"));
                         System.out.println("rip::::::::::: " + help.length );
                         System.out.println("nao percebo nada disto " + help[0].length());
-                  }
+                  }*/
 
             }
 
