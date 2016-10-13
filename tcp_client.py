95950643a538f683c6c1622e9077506520151564
nadia-mourao <nadialm_16@hotmail.com>
2016-10-13 12:26:03 +0100

    python is more fluffy

---
+++ tcp_client.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+ 
+import socket
+import sys
+ 
+
+
+TCP_IP = '194.210.159.18'
+TCP_PORT = 58045
+BUFFER_SIZE = 1024
+split_space = " "
+message = "cucu"
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+print 'try' , sys.argv[1]
+
+
+input_user = raw_input()
+print input_user
+input_split = input_user.split()
+
+print 'split: ', input_split , ' -- ', int(input_split[1])
+
+if input_split[0]=='request':
+	if input_split[2]=='t':
+		palavras=input_split[3:]
+		if (int(input_split[1]))==len(palavras):
+			message = 'TRQ t ' + input_split[1]
+			for p in palavras:
+				message += ' ' + p
+			message += '\n'
+
+				
+
+	elif input_split[2]=='f':
+		message='lol'
+	else:
+		message = 'fuckkkkkkk'
+	
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(message)
+data = s.recv(BUFFER_SIZE)
+s.close()
+ 
+print "received data:", data
+
+sys.exit('DONE CARALHO')
