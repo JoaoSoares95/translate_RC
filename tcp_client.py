6a6b588bada0c7902409fddd8f9ea72944e4f746
nadia-mourao <nadialm_16@hotmail.com>
2016-10-13 15:09:13 +0100

    argv a funcionar

--- tcp_client.py
+++ tcp_client.py
@@ -2,51 +2,74 @@
  
 import socket
 import sys
+import fcntl
+import struct
  
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
-
-TCP_IP = '194.210.159.18'
+TCP_IP = get_ip_address()
 TCP_PORT = 59000
 BUFFER_SIZE = 1024
-split_space = " "
-message = "cucu"
+message = ""
+
+size=len(sys.argv)
+
+if size == 3 or size == 5:
+	for i in range(1,size,2):
+		if sys.argv[i]=='-n':
+			TCP_IP=sys.argv[i+1]
+		elif sys.argv[i]=='-p':
+			TCP_PORT=sys.argv[i+1]
+
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
-print 'try' , sys.argv[1]
+print 'try' , get_ip_address()
+input_user=''
 
 
-input_user = raw_input()
-print input_user
-input_split = input_user.split()
-
-print 'split: ', input_split , ' -- ', int(input_split[1])
 while 1:
-	if input_split[0]=='request':
-		if input_split[2]=='t':
-			palavras=input_split[3:]
-			if (int(input_split[1]))==len(palavras):
-				message = 'TRQ t ' + input_split[1]
+	while 1:
+		input_user = raw_input()
+		print input_user, ' -- ', len(input_user)
+		input_split = input_user.split()
+
+		if input_split[0]=='request':
+			if (len(input_split)) > 2 and input_split[2]=='t':
+				palavras=input_split[3:]
+				message = 'TRQ t ' + str(len(palavras))
 				for p in palavras:
-					message += ' ' + p
+					if p!='':
+						message += ' ' + p
 				message += '\n'
 				break
 
-		elif input_split[2]=='f':
-			if (int(input_split[1])==1 :
-				message = 'TRQ f ' + input_split[3]
-				break
-	
-	else:
-		print 'Dados errados, tente outra vez:\n'
-	
+			elif len(input_split) > 2 and input_split[2]=='f':
+				if (int(input_split[1])==1) :
+					message = 'TRQ f ' + input_split[3]
+					break
+			else:
+				print 'Dados errados, tente outra vez:'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(message)
-data = s.recv(BUFFER_SIZE)
-s.close()
+		
+		
+		else:
+			print 'Dados errados, tente outra vez:'
+			
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(message)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
  
 print "received data:", data
 
 sys.exit('DONE CARALHO')
+
+	
+
+
