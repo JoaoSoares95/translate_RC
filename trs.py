8361470c1d289251f97e25ef7a66203406daa65f
JoaoSoares95 <joao_soares_95@hotmail.com>
2016-10-14 17:27:23 +0100

    Estrutura do programa em cobra, mas nao acabado obviamente

--- trs.py
+++ trs.py
@@ -14,10 +14,10 @@ def get_ip_address():
 
 #variable defining
 TCP_IP = get_ip_address()
-TCS_PORT = 59000
+TRS_PORT = 59000
 UDP_IP = TCP_IP
-TRS_PORT = 58045
-BUFFER_SIZE = 1024
+TCS_PORT = 58045
+BUFFER_SIZE = 10240
 message = ""
 languagues = []
 n_lan = 0
@@ -29,127 +29,177 @@ if size <= 1:
 else:
 	lang = sys.argv[1]
 
+print lang
 
 		
 #Verificacao de input
-if size == 2 or size == 4 or size == 6 or 8:
+if size == 2 or size == 4 or size == 6 or size == 8:
 	print "entroiu"
-	for i in range( 1 , size , 2 ):
+	for i in range( 2 , size , 2 ):
 		if sys.argv[i] == '-n':
 			UDP_IP = sys.argv[i+1]
 			print UDP_IP
 		elif sys.argv[i] == '-p':
-			UDP_PORT = int( sys.argv[i+1] )
+			TRS_PORT = int( sys.argv[i+1] )
 			print UDP_PORT
 		elif sys.argv[i] == "-e":
 			TCS_PORT = int( sys.argv[i+1] )	
+			print TCS_PORT
 else:
 	print "Creation of Server Abort"
 
-#output to socket defining
-message = "SRG " + lang + " " + TCP_IP + " " + str(TCS_PORT) + "\n"
+###############################Avisar o TCS que iniciamos#####################################
+
+message = "SRG " + lang + " " + TCP_IP + " " + str(TCS_PORT)
+print message
+
+#Socket UDP para TCS
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(message,(UDP_IP,TRS_PORT))
+s.sendto(message,(UDP_IP,TCS_PORT))
 data = s.recv(BUFFER_SIZE)
-s.close()
+
 print "received data:", data
+s.close()
 
+###############################################################################################
 
-#Verifications
+#######################################Verificacoes---------- apagar##################################
+
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 print 'try' , get_ip_address()
 input_user=''
 
-#cycle to communicate
+######################################################################################################
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+############################################Ciclo infinito para comunicacao###################################
 while 1:
-	#input
-	input_user = raw_input()
-	print input_user, ' -- ', len(input_user)
-	input_split = input_user.split()
 
-	if input_split[0]=='list':
-		message = "ULQ\n"
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.sendto(message,(UDP_IP,TRS_PORT))
-		data = s.recv(BUFFER_SIZE)
-		s.close()
-		print "received data:", data
-
-		input_split = data.split()
-
-		print "lista: ", input_split
-		if (len(input_split)-2) == int(input_split[1]) and input_split[0]=='ULR' :
-			n_lan = int(input_split[1])
-			languagues = input_split[2:]
-			i=1
-			for l in languagues:
-				print i, ' ', l
-				i+=1
-		else:
-			print 'Mensagem errada do TCS'
-
-
-	elif input_split[0]=='request':
-		n = int(input_split[1])
-		if n > 0 and n <= n_lan :
-			print "fcfgvbhnj", n_lan-1
-			message = "UNQ " + languagues[ int(input_split[1]) - 1 ] + "\n"
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s.sendto(message,(UDP_IP,UDP_PORT))
-			data = s.recv(BUFFER_SIZE)
-			s.close()
-			print "received data:", data
-
-			data_split = data.split() 
-		
-		if (len(input_split)) > 2 and input_split[2]=='t' and data_split[0] == "UNR" :
-			TCP_IP=data_split[1]
-			print TCP_IP
-			TCs_PORT=int(data_split[2])
-			print TCS_PORT
-			palavras=input_split[3:]
-			message = 'TRQ t ' + str(len(palavras))
-			for p in palavras:
-				if p!='':
-					message += ' ' + p
-			message += '\n'
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((TCP_IP, TCS_PORT))
-			s.send(message)
-			data = s.recv(BUFFER_SIZE)
-			s.close()
-			print "received data:", data
-
-
-		elif len(input_split) > 2 and input_split[2]=='f':
-			TCP_IP=data_split[1]
-			print TCP_IP
-			TCS_PORT=int(data_split[2])
-			ola=input_split[3]
-			f = open(ola,'rb')
-			l = f.read(BUFFER_SIZE)
-			
-			
-			#if (int(input_split[1])==1) :
-			message = 'TRQ f ' + input_split[3]+ " " + str(os.stat(ola).st_size ) + " " + l +"\n"
-
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((TCP_IP, TCS_PORT))
-			s.send(message)
-			s.send(l)
-
-			data = s.recv(BUFFER_SIZE)
-			s.close()
-			print "received data:", data
-
-		else:
-			print 'Dados errados, tente outra vez:'
-
-	else:
-		print 'Dados errados, tente outra vez:'
-		
+	s.bind((TCP_IP, TCS_PORT))
+	s.listen(1)
 	
+	addr = s.accept()
+	print addr
+	
+	data = s.recv(BUFFER_SIZE)
+	print data
+	
+	input_split = data.split()
+	print "Splitted: ", input_split
+
+	#Verifica se o cliente manda comando certo
+	if input_split[0] == "TRQ":
+		if len(input_split) >= 4:
+		
+			if input_split[1] == "t":
+				words = input_split[3:]
+				print words
+			
+			elif input_split[1] == "f":
+				file_name = input_split[2]
+				file_size = input_split[3]
+				file_data = input_split[4]
+				
+			print file_name
+			print file_size
+			print file_data
+		else:
+			s.sendto("TRR ERR",(UDP_IP,TCS_PORT))
+
+		
+	#print data
+	#s.close()
+	
+
+	
+	#Ve se o cliente esta a pedir a lista de linguas corretamente
+	# if input_split[0]=='ULQ' and len(input_split) == 1:
+		
+		# message = "ULQ\n"
+		# print message
+		# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		# s.sendto(message,(UDP_IP,TRS_PORT))
+		# data = s.recv(BUFFER_SIZE)
+		# s.close()
+		# print "received data:", data
+
+		# input_split = data.split()
+
+		# print "lista: ", input_split
+		# if (len(input_split)-2) == int(input_split[1]) and input_split[0]=='ULR' :
+			# n_lan = int(input_split[1])
+			# languagues = input_split[2:]
+			# i=1
+			# for l in languagues:
+				# print i, ' ', l
+				# i+=1
+		# else:
+			# print 'Mensagem errada do TCS'
+
+
+	# elif input_split[0]=='request':
+		# n = int(input_split[1])
+		# if n > 0 and n <= n_lan :
+			# print "fcfgvbhnj", n_lan-1
+			# message = "UNQ " + languagues[ int(input_split[1]) - 1 ] + "\n"
+			# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			# s.sendto(message,(UDP_IP,UDP_PORT))
+			# data = s.recv(BUFFER_SIZE)
+			# s.close()
+			# print "received data:", data
+
+			# data_split = data.split() 
+		
+		# if (len(input_split)) > 2 and input_split[2]=='t' and data_split[0] == "UNR" :
+			# TCP_IP=data_split[1]
+			# print TCP_IP
+			# TCs_PORT=int(data_split[2])
+			# print TCS_PORT
+			# palavras=input_split[3:]
+			# message = 'TRQ t ' + str(len(palavras))
+			# for p in palavras:
+				# if p!='':
+					# message += ' ' + p
+			# message += '\n'
+			# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			# s.connect((TCP_IP, TCS_PORT))
+			# s.send(message)
+			# data = s.recv(BUFFER_SIZE)
+			# s.close()
+			# print "received data:", data
+
+
+		# elif len(input_split) > 2 and input_split[2]=='f':
+			# TCP_IP=data_split[1]
+			# print TCP_IP
+			# TCS_PORT=int(data_split[2])
+			# ola=input_split[3]
+			# f = open(ola,'rb')
+			# l = f.read(BUFFER_SIZE)
+			
+			
+			# #if (int(input_split[1])==1) :
+			# message = 'TRQ f ' + input_split[3]+ " " + str(os.stat(ola).st_size ) + " " + l +"\n"
+
+			# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			# s.connect((TCP_IP, TCS_PORT))
+			# s.send(message)
+			# s.send(l)
+
+			# data = s.recv(BUFFER_SIZE)
+			# s.close()
+			# print "received data:", data
+
+		# else:
+			# print 'Dados errados, tente outra vez:'
+
+	# else:
+		# print 'Dados errados, tente outra vez:'
+		
+	s.close()
+#################################################################################################################	
  
 
 sys.exit('DONE CARALHO')
